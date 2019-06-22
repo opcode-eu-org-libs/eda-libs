aed5aff59632a3d22d3f2b2674f0c3763071eed4
Robert Paciorek <robert@opcode.eu.org>
2019-06-22 14:25:25 +0000

    fix gEDA config to use elements from main libs dir
    Use extra "component-library" directive to include symbols located
    directly in .gEDA/Components and schematics located directly in
    .gEDA/Sources/ and .gEDA/Sources.EDA-libs/

--- configs/libs.01.scm
+++ configs/libs.01.scm
@@ -1,11 +1,16 @@
+(display "load libs.01.scm\n")
+
 ;; use ONLY own component symbols library
 (reset-component-library)
 (component-library-search (build-path (getenv "HOME") ".gEDA/Components.EDA-libs/"))
 (component-library-search (build-path (getenv "HOME") ".gEDA/Components.gEDA/"))
+(component-library        (build-path (getenv "HOME") ".gEDA/Components/") "user libs")
 (component-library-search (build-path (getenv "HOME") ".gEDA/Components/"))
 
 ;; use ALSO own schematic source library
+(source-library           (build-path (getenv "HOME") ".gEDA/Sources/"))
 (source-library-search    (build-path (getenv "HOME") ".gEDA/Sources/"))
+(source-library           (build-path (getenv "HOME") ".gEDA/Sources.EDA-libs/"))
 (source-library-search    (build-path (getenv "HOME") ".gEDA/Sources.EDA-libs/"))
 
 ;; search for symbols and souyrces in current working directory
